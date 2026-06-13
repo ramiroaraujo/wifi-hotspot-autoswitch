ada24e9797f5bd811dac3a8b9b33cfb1130b8704
Ramiro Araujo <rama.araujo@gmail.com>
2026-06-13 15:43:20 -0300

    Set version to 0.1

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ cat > "$APP/Contents/Info.plist" <<PLIST
   <key>CFBundleIdentifier</key><string>${ID}</string>
   <key>CFBundleName</key><string>WifiAutoswitch</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>1.0</string>
+  <key>CFBundleShortVersionString</key><string>0.1</string>
   <key>CFBundleVersion</key><string>1</string>
   <key>LSMinimumSystemVersion</key><string>14.0</string>
   <key>LSUIElement</key><true/>
